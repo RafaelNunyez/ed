f7236ce6192f7a0d17d1dfc11147172c04969fba
RafaelNunyez <rafa97burja@gmail.com>
2018-06-03 13:54:16 +0200

    Added buttons listeners

--- EDProject/app/src/main/java/com/example/rafael/edproject/MainActivity.java
+++ EDProject/app/src/main/java/com/example/rafael/edproject/MainActivity.java
@@ -2,12 +2,82 @@ package com.example.rafael.edproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText num1, num2;
+    private Button sumaBtn, restaBtn, multBtn, divBtn;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        sumaBtn = findViewById(R.id.suma);
+        restaBtn = findViewById(R.id.resta);
+        multBtn = findViewById(R.id.mult);
+        divBtn = findViewById(R.id.div);
+        resultado = findViewById(R.id.resultado);
+
+        sumaBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String value1 = num1.getText().toString();
+                String value2 = num2.getText().toString();
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
+                int res = a + b;
+                resultado.setText(res);
+            }
+        });
+
+        restaBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String value1 = num1.getText().toString();
+                String value2 = num2.getText().toString();
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
+                int res = a - b;
+                resultado.setText(res);
+            }
+        });
+
+        multBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String value1 = num1.getText().toString();
+                String value2 = num2.getText().toString();
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
+                int res = a * b;
+                resultado.setText(res);
+            }
+        });
+
+        divBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String value1 = num1.getText().toString();
+                String value2 = num2.getText().toString();
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
+                int res = a / b;
+                resultado.setText(res);
+            }
+        });
+
     }
 }
